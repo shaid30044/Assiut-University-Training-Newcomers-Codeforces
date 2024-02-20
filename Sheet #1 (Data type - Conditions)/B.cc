68d68e62f75b12c68ac27e30137f4e57d15068be
shaid30044 <kaona30044@gmail.com>
2024-02-20 10:19:18 +0600

    Basic Data Types

---
+++ Sheet #1 (Data type - Conditions)/B.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    long long l;
+    char ch;
+    float f;
+    double d;
+
+    cin >> i >> l >> ch >> f >> d;
+    cout << i << endl
+         << l << endl
+         << ch << endl
+         << f << endl
+         << d << endl;
+
+    return 0;
+}
